f093beee63f2acc05e70cf4e35c6a83a78307fad
Aarushthegamemaster <aarushchandra0604@gmail.com>
2025-03-22 18:23:42 -0700

    Operators_are_cool

---
+++ AND_OR_operator.py
@@ -0,0 +1,19 @@
+#And operator
+a = 10  
+b = 12
+c = 0
+
+if a and b and c:
+    print("Their boolean value is true")
+else:
+    print("Their boolean value is false")
+
+#Or operator
+d = -10
+e = 10
+f = 0
+
+if d > 0 or e > 0:
+    print("Either of these numbers are greater than 0")
+else:
+    print("Both are not greater than 0")
