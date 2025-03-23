b6fff074b47364123b73eec58cb8e0d9b3d9e740
Aarushthegamemaster <aarushchandra0604@gmail.com>
2025-03-22 18:38:31 -0700

    NOT_EQUAL

---
+++ Not_equal.py
@@ -0,0 +1,22 @@
+a = 82
+b = 45
+c = 68
+
+print(a != b)
+print(b != c)
+
+d = "python"
+e = "coding"
+
+if d != e:
+    print(d, "is not the same as", e)
+else:
+    print(d, "and", e,"are the same")
+
+
+f = 4
+g = 5 
+
+if (f==1) != (g==5):
+    print("Hello")
+
